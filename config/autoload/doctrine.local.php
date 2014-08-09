2f9d7eaf6c3f399096e96014da62426c28426da2
gutorockstar <augustoalvess@hotmail.com>
2014-08-08 22:13:14 -0300

    Feitas modificações para fazer com que o site trabalhe com postgres

--- config/autoload/doctrine.local.php
+++ config/autoload/doctrine.local.php
@@ -11,12 +11,13 @@ return array(
     'doctrine' => array(
         'connection' => array(
             'orm_default' => array(
-                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                //'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOPgSql\Driver',
                 'params' => array(
                     'host'     => 'localhost',
-                    //'port'     => '5432',
-                    'user'     => 'root',
-                    'password' => 'root',
+                    'port'     => '5432',
+                    'user'     => 'postgres',
+                    'password' => 'postgres',
                     'dbname'   => 'site',
                 )
             )
