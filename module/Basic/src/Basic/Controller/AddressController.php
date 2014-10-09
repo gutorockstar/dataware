e246b20dcb3d1fecc5a6be882c65dbebfe8b0a66
gutorockstar <augustoalvess@hotmail.com>
2014-10-09 17:02:20 -0300

    Criando as entidades do módulo básico

---
+++ module/Basic/src/Basic/Controller/AddressController.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AddressController
+ *
+ * @author augusto
+ */
+class AddressController 
+{
+    
+}
+
+?>
