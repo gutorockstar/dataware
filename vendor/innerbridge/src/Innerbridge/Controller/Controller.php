001a9f5ec9c3128edf8ba66d6062c2fc6aeecf1b
gutorockstar <augustoalvess@hotmail.com>
2014-12-17 11:50:36 -0200

    Ajustado

--- vendor/innerbridge/src/Innerbridge/Controller/Controller.php
+++ vendor/innerbridge/src/Innerbridge/Controller/Controller.php
@@ -15,6 +15,7 @@ namespace Innerbridge\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\Mvc\MvcEvent;
 use Zend\View\Helper\ServerUrl;
+use Zend\View\Model\ViewModel;
 
 class Controller extends AbstractActionController
 {
@@ -72,6 +73,54 @@ class Controller extends AbstractActionController
         
         return $currentUrl;
     }
+    
+    public function indexAction()
+    {
+        
+    }
+    
+    public function searchAction()
+    {
+        return new ViewModel();
+    }
+    
+    public function crudAction()
+    {
+        
+    }
+    
+    public function newAction()
+    {
+        return new ViewModel();
+    }
+    
+    public function editAction()
+    {
+        $data = $this->getRequest()->getPost();
+        
+        return new ViewModel();
+    }
+    
+    public function saveAction()
+    {
+        $data = $this->getRequest()->getPost();
+    }
+    
+    public function deleteAction()
+    {
+        $data = $this->getRequest()->getPost();
+    }
+    
+    public function printAction()
+    {
+        
+    }
+    
+    public function backAction()
+    {
+        
+    }
+    
 }
 
 ?>
