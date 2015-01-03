4a772ed65398e233d13577f1d74c78ec0494d83e
gutorockstar <augustoalvess@hotmail.com>
2015-01-03 12:16:34 -0200

    Ajsutado

--- data/cache/SynergyDataGrid/orm_entity_classmap.php
+++ data/cache/SynergyDataGrid/orm_entity_classmap.php
@@ -1,5 +1,11 @@
 <?php return array (
+  'action' => 'Admin\\Entity\\Action',
   'country' => 'Basic\\Entity\\Country',
-  'login' => 'Admin\\Entity\\Login',
+  'group-user' => 'Admin\\Entity\\GroupUser',
+  'module' => 'Admin\\Entity\\Module',
+  'route' => 'Admin\\Entity\\Route',
   'state' => 'Basic\\Entity\\State',
+  'user-account' => 'Admin\\Entity\\UserAccount',
+  'user-group' => 'Admin\\Entity\\UserGroup',
+  'user-or-group-action-on-route' => 'Admin\\Entity\\UserOrGroupActionOnRoute',
 );
--- module/Basic/src/Basic/Controller/StateController.php
+++ module/Basic/src/Basic/Controller/StateController.php
@@ -12,15 +12,29 @@ namespace Basic\Controller;
 use Admin\Controller\Controller;
 
 class StateController extends Controller
-{        
+{    
+    public function indexAction()
+    {
+        //$this->redirect()->toRoute($this->getCurrentRoute(), array('action' => 'search'));
+        
+        $grid = $this->getServiceLocator()->get(parent::SERVICE_JQGRID_LOCATOR)->setGridIdentity($this->getCurrentEntity());
+        $grid->setUrl('/basic/state/crud');
+
+        return array('grid' => $grid);
+    }
+    
     /**
-     * Tela de busca por estado
+     * Executa as ações de inserção, edição, exclusão, e busca.
      * 
-     * @return \Zend\View\Model\ViewModel
+     * @param array $options
      */
     public function crudAction()
-    {   
+    {
         $options['fieldName'] = $this->params()->fromRoute('country', null);
-        parent::crudAction($options);
+        $grid = $this->getServiceLocator()->get(parent::SERVICE_JQGRID_LOCATOR)->setGridIdentity($this->getCurrentEntity());
+        $response = $grid->prepareGridData($this->getRequest(), $options);
+
+        echo json_encode($response);
+        exit;
     }
 }
