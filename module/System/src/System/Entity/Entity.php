ad31d4758ab26bfba61568f04fd67e985e7dcf1b
root <root@augusto-desktop.(none)>
2015-03-04 16:57:28 -0300

    Ajustado

---
+++ module/System/src/System/Entity/Entity.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Entity
+ *
+ * @author augusto
+ */
+namespace System\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Zend\Form\Annotation;
+
+class Entity 
+{
+    
+}
+
+?>
