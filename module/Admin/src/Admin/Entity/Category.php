54d92a78611947f0cda5f90ec822113560e8693a
root <root@augusto-desktop.(none)>
2015-03-11 17:46:25 -0300

    Ajustado

--- module/Admin/src/Admin/Entity/Category.php
+++ module/Admin/src/Admin/Entity/Category.php
@@ -35,6 +35,17 @@ class Category
     protected $id;
     
     /**
+     * @ORM\ManyToOne(targetEntity="Category", cascade={"all"}, fetch="EAGER")
+     * @ORM\JoinColumn(name="categoryfather_id", referencedColumnName="id", nullable=true)
+     * 
+     * @Annotation\Type("Zend\Form\Element\Select")
+     * @Annotation\Filter({"name":"StripTags"})
+     * @Annotation\Options({"label":"Categoria pai"})
+     * @Annotation\Attributes({"class":"input-text form-control"})
+     */
+    protected  $categoryfather;
+    
+    /**
      * @ORM\Column(type="string", length=45, columnDefinition="VARCHAR(45) NOT NULL")
      * 
      * @Annotation\Type("Zend\Form\Element\Text")
@@ -42,17 +53,47 @@ class Category
      * @Annotation\Options({"label":"Título"})
      * @Annotation\Attributes({"class":"input-text form-control"})
      * @Annotation\Validator({"name":"StringLength", "options":{"min":1, "max":45}})
-     * @Annotation\ErrorMessage("O valor para 'Nome' é requerido.");
+     * @Annotation\ErrorMessage("O valor para 'Nome' é requerido.")
      */
     protected $title;
     
+    /**
+     * @ORM\Column(type="string", length=255, columnDefinition="VARCHAR(255)")
+     * 
+     * @Annotation\Type("Zend\Form\Element\File")
+     * @Annotation\Filter({"name":"StripTags"})
+     * @Annotation\Options({"label":"Imagem de capa"})
+     * @Annotation\Attributes({"class":"input-text form-control"})
+     */
+    protected $cover;
+    
+    /**
+     * @ORM\Column(type="text", columnDefinition="TEXT")
+     * 
+     * @Annotation\Type("Zend\Form\Element\Textarea")
+     * @Annotation\Filter({"name":"StripTags"})
+     * @Annotation\Options({"label":"descrição"})
+     * @Annotation\Attributes({"class":"input-text form-control"})
+     */
+    protected $description;
+    
+    /**
+     * @ORM\Column(type="boolean", columnDefinition="BOOLEAN NOT NULL DEFAULT TRUE")
+     * 
+     * @Annotation\Type("Zend\Form\Element\Radio")
+     * @Annotation\Filter({"name":"StripTags"})
+     * @Annotation\Options({"label":"Status", "value_options":{"t":"Ativo", "f":"Inativo"}})
+     * @Annotation\Attributes({"class":"input-text form-control"})
+     */
+    protected $status;
+
     /**
      * @Annotation\Type("Zend\Form\Element\Submit")
      * @Annotation\Attributes({"value":"Confirmar", "class":"input-submit btn btn-primary"})
      */
     protected $send;
     
-    public function getId()
+    public function getId() 
     {
         return $this->id;
     }
@@ -60,6 +101,16 @@ class Category
     public function setId($id) 
     {
         $this->id = $id;
+    }
+
+    public function getCategoryfather() 
+    {
+        return $this->categoryfather;
+    }
+
+    public function setCategoryfather($categoryfather) 
+    {
+        $this->categoryfather = $categoryfather;
     }
 
     public function getTitle() 
@@ -71,15 +122,35 @@ class Category
     {
         $this->title = $title;
     }
-    
-    public function getStates() 
+
+    public function getCover() 
     {
-        return $this->states;
+        return $this->cover;
     }
 
-    public function setStates($states) 
+    public function setCover($cover) 
     {
-        $this->states = $states;
+        $this->cover = $cover;
+    }
+
+    public function getDescription() 
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description) 
+    {
+        $this->description = $description;
+    }
+
+    public function getStatus() 
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status) 
+    {
+        $this->status = $status;
     }
 }
 
