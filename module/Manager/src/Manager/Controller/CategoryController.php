775950d218f2566cde7e35b92c5b9890fc2f9ed2
gutorockstar <augustoalvess@hotmail.com>
2015-04-22 20:01:20 -0300

    Ajsutado

--- module/Manager/src/Manager/Controller/CategoryController.php
+++ module/Manager/src/Manager/Controller/CategoryController.php
@@ -17,8 +17,19 @@ use Manager\Entity\Category;
 
 class CategoryController extends CrudController
 {   
-    
-    
+    public function getListValuesToSelectElement($element)
+    {
+        $entity = $element->getOption('entity');
+        
+        $repository = $this->getObjectManager()->getRepository($entity);
+        $query = $repository->createQueryBuilder('list')
+                            ->select("list.id, list.title")
+                            ->andWhere("list.categoryfather IS NULL")
+                            ->orderBy("list.title")
+                            ->getQuery();        
+        
+        return $query->getResult();
+    }
 }
 
 ?>
