adf8ddc3ded5e50e89d219c629e1b3d237257ceb
gutorockstar <augustoalvess@hotmail.com>
2015-04-22 20:50:31 -0300

    Ajustado

---
+++ module/Manager/src/Manager/Controller/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UserController
+ *
+ * @author augusto
+ */
+namespace Manager\Controller;
+
+use System\Controller\CrudController;
+
+class UserController extends CrudController
+{
+    
+}
+
+?>
